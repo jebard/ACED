2da3a768effd2fdde2825c017a4c05bd6f501fbc
Jonathan Bard <jbard@localhost>
2022-12-29 18:27:11 -0500

    first commit

---
+++ R/deconvolution.R
@@ -0,0 +1,23 @@
+# Deconvolution Strategy Pattern
+# Ingests a request to run deconvolution, returns a normalized result regardless of strategy
+# @author jbard
+#
+
+run_deconvolution <- function(strategy,){
+  if(strategy="bayesprism"){
+    run_bayesprism()
+  } else if (strategy=="music") {
+    run_music()
+  } else if (strategy=="gedit"){
+    run_gedit()
+  } else {
+    warning("Invalid deconvolution strategy specified")
+  }
+}
+
+run_music <-function(){}
+run_bayesprism <-function(){}
+run_gedit <-function(){}
+
+
+
---
+++ R/utility_functions.R
@@ -0,0 +1,15 @@
+# Generate Cluster Statistics
+# @author jbard
+#
+
+#' calc_cluster_proportions
+#'
+#' @return
+#' @export
+#'
+#' @examples
+calc_cluster_proportions <- function() {
+
+}
+
+
