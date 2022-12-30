c8d6f41f9ddd21b3233085300e9d37407d53f3b5
Jonathan Bard <jbard@buffalo.edu>
2022-12-29 20:04:46 -0500

    generalized object names

--- R/gedit_strategy.R
+++ R/gedit_strategy.R
@@ -2,7 +2,7 @@
 ### GEDIT is a python based tool that solves deconvolution using  ...
 ###
 ### python2.7 GEDIT2.py -mix MixtureFullRefOrig.csv  -ref FullRefOrig.csv
-
+library(Seurat)
 run_gedit <- function(ref_obj,query_obj){
   message("Preparing the reference object for GEDIT")
   gedit_prep_reference()
@@ -23,7 +23,7 @@ gedit_prep_reference <- function(ref_obj){
 
 gedit_prep_query <- function(query_obj){
   ### save out the mixture sets for each original sample
-  Idents(combined.seurat.sct) <- "orig.ident"
+  Idents(query_obj) <- "orig.ident"
   write.csv(file="MixtureQuery.csv",AverageExpression(combined.seurat.sct,assays = "RNA",slot = "counts")$RNA)
 }
 
@@ -41,8 +41,8 @@ execute_gedit <- function(){
 
 gedit_gather_results <- function(){
   predictions = read.table(file="MixtureQuery.csv_RefObj.csv_50_Entropy_0.0_CTPredictions.tsv",header = TRUE, row.names = 1, sep = "\t")
-  truth <- prop.table(table(combined.seurat.sct$orig.ident,combined.seurat.sct$seurat_clusters),margin=1)
-  AE <- truth - predictions[rownames(truth),]
+  #truth <- prop.table(table(combined.seurat.sct$orig.ident,combined.seurat.sct$seurat_clusters),margin=1)
+  #AE <- truth - predictions[rownames(truth),]
   return(predictions)
 }
 
