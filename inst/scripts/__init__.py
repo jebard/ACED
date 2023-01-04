204feeaf17939a1cdb5e259fb6ba2ec97cf09218
Jonathan Bard <jbard@buffalo.edu>
2023-01-03 21:58:16 -0500

    updates

--- inst/scripts/__init__.py
+++ inst/scripts/__init__.py
@@ -1,7 +1,7 @@
 import sys
-from scripts import getSigGenesModal
-from scripts import MatrixTools
-from scripts import HandleInput as HandleInput
+import getSigGenesModal
+import MatrixTools
+import HandleInput as HandleInput
 import os
 import random
 import numpy as np
