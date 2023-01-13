71c4935c7ac75643874f1af0b5a560c1ae016302
Jonathan Bard <jbard@buffalo.edu>
2023-01-13 14:43:13 -0500

    added ace boot

--- R/deconvolution.R
+++ R/deconvolution.R
@@ -101,7 +101,7 @@ evaluate_deconvolution <- function(ref_obj, query_obj, strategy){
   message("Bootstrapping the random ACE background calculation finished")
 
   message("Deconvolution results in: ",MAE,",",RSE,",",SMAPE,",",RMSE,",",AVP_Z,",",EVP_Z,",",AE,",",AE_CC,",",LM,",",ACE,ACE_Random,MAE_RANDOM)
-  return(c(MAE,RSE,SMAPE,RMSE,AVP_Z,EVP_Z,AE,AE_CC,LM,ACE,ACE_Random,MAE_RANDOM))
+  return(c(MAE,RSE,SMAPE,RMSE,AVP_Z,EVP_Z,AE,AE_CC,LM,ACE,ACE_Random,MAE_RANDOM,ACE_Boot))
 }
 
 get_random_proportions <- function(ref_obj){
