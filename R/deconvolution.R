1b8cc43137d03ba8add4963fec17e5c77d053608
Jonathan Bard <jbard@buffalo.edu>
2023-02-03 16:51:12 -0500

    handles a case where MUSIC returns things out of order

--- R/deconvolution.R
+++ R/deconvolution.R
@@ -98,7 +98,11 @@ evaluate_deconvolution <- function(ref_obj, query_obj, strategy){
     rownames(estimated_proportions) <- rownames(actual_proportion)
   }
 
+  paste0("x",colnames(actual_proportion))
+  ### enforce the same column order
   estimated_proportions <- estimated_proportions[rownames(actual_proportion),]
+  estimated_proportions <- estimated_proportions[,colnames(actual_proportion)]
+
   print("Estimated:")
   print(estimated_proportions)
 
