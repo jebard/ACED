9fea7a755e81f762a93d7d803a4fad3baa4b69dd
Jonathan Bard <jbard@buffalo.edu>
2023-02-02 21:14:40 -0500

    edits

--- R/deconvolution.R
+++ R/deconvolution.R
@@ -23,6 +23,7 @@ DRRSD <- function(ref_obj=ref_obj,query_obj=query_obj,start=0.01,stop=1,step=.05
       }
     #}
     gedit_results <- evaluate_deconvolution(ref_obj,query_obj,"gedit")
+
     print(paste0("Res:",res,",",length(levels(ref_obj$seurat_clusters)),",",gedit_results))
 
 #    return(c(MAE,RSE,SMAPE,RMSE,AVP_Z,
@@ -41,8 +42,7 @@ DRRSD <- function(ref_obj=ref_obj,query_obj=query_obj,start=0.01,stop=1,step=.05
     values_ACE = c(values_ACE,gedit_results[10])
     values_ACE_random = c(values_ACE_random,gedit_results[11])
     values_MAE_random = c(values_MAE_random,gedit_results[12])
-    ### ignore [13] ace boot
-    values_PC = c(values_PC,gedit_results[14])
+    values_PC = c(values_PC,gedit_results[13])
     clusters = c(clusters,length(levels(ref_obj$seurat_clusters)))
     resolution = c(resolution,res)
     plot(values_ACE_random~resolution,col="red",ylim=c(0,max(values_ACE_random)))
@@ -103,7 +103,8 @@ evaluate_deconvolution <- function(ref_obj, query_obj, strategy){
   LM <- calculate_linear_regression(actual_proportion,estimated_proportions)
   ACE <- calculate_absolute_cell_error(ref_obj,actual_proportion,estimated_proportions)
   PC <- calculate_proportinal_correlation(ref_obj,actual_proportion,estimated_proportions)
-  message("Bootstrapping 1000 times a random background calculation")
+
+   message("Bootstrapping 500 times a random background calculation")
   ACE_Boot <- c()
   for (boot in seq(1,500)){
   ACE_Boot <- rbind(ACE_Boot,
@@ -116,11 +117,11 @@ evaluate_deconvolution <- function(ref_obj, query_obj, strategy){
 
   message("Deconvolution results in: ",MAE,",",RSE,",",SMAPE,",",
           RMSE,",",AVP_Z,",",EVP_Z,",",AE,",",AE_CC,",",LM,",",
-          ACE,ACE_Random,MAE_RANDOM,", ",PC)
+          ACE,",",ACE_Random,",",MAE_RANDOM,", PC:",PC)
 
   return(c(MAE,RSE,SMAPE,RMSE,AVP_Z,
            EVP_Z,AE,AE_CC,LM,ACE,
-           ACE_Random,MAE_RANDOM,ACE_Boot,PC))
+           ACE_Random,MAE_RANDOM,PC))
 }
 
 get_random_proportions <- function(ref_obj){
