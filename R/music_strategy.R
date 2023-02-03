628773396aca28df2de3db5484e5090d743a66ec
Jonathan Bard <jbard@buffalo.edu>
2023-02-03 16:21:04 -0500

    returning just the prop weight for music

--- R/music_strategy.R
+++ R/music_strategy.R
@@ -19,7 +19,7 @@ run_music <- function(ref_object){
                               sc.eset = SC.eset,
                               clusters = 'cellType',
                               samples = 'SubjectName', verbose = T)
-  return(estimated.prop)
+  return(estimated.prop$Est.prop.weighted)
 }
 
 music_prep_reference <- function(music.input=NULL){
