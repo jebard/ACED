264d08a393161e9b9555cb5a0c980ce36001cbe3
Jonathan Bard <jbard@buffalo.edu>
2023-02-03 20:01:44 -0500

    Testing random permutation of the rows background to see what happens. This should preserve the distribution of the original set

--- R/utility_functions.R
+++ R/utility_functions.R
@@ -12,4 +12,11 @@ calc_cluster_proportions <- function() {
 
 }
 
+### https://rdrr.io/github/statlab/permuter/src/R/utils.R
+permute_within_rows <- function(x) {
+  for (row in seq_len(nrow(x))) {
+    x[row, ] <- sample(x[row, ])
+  }
+  return(x)
+}
 
