32f3c99727e9e14857bba624e4b467e09daf8e97
Jonathan Bard <jbard@buffalo.edu>
2023-02-28 13:32:01 -0500

    level missing in music

--- R/deconvolution.R
+++ R/deconvolution.R
@@ -124,7 +124,7 @@ evaluate_deconvolution <- function(ref_obj, query_obj, strategy){
   if(strategy == "music"){
     ### for some bizarre reason, MUSIC will return clusters out of order
     ### If a given cluster isn't in the subset, set estimated proportion to zeros
-    print(colnames(estimated_proportions))
+    print(levels(ref_obj$seurat_clusters)[!(levels(ref_obj$seurat_clusters) %in% colnames(estimated_proportions))])
     for (clust in levels(ref_obj$seurat_clusters)[!(levels(ref_obj$seurat_clusters) %in% colnames(estimated_proportions))]) {
       estimated_proportions[, clust] <- 0.0
     }
