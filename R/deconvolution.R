671d33154a136d4baf93c51dccb4fffcf540a16c
Jonathan Bard <jbard@buffalo.edu>
2023-02-28 13:48:23 -0500

    try again

--- R/deconvolution.R
+++ R/deconvolution.R
@@ -127,7 +127,7 @@ evaluate_deconvolution <- function(ref_obj, query_obj, strategy){
     #print(levels(ref_obj$seurat_clusters)[!(levels(ref_obj$seurat_clusters) %in% colnames(estimated_proportions))])
     for (clust in levels(ref_obj$seurat_clusters)[!(levels(ref_obj$seurat_clusters) %in% colnames(estimated_proportions))]) {
       print(paste0("Adding missing cluster: ",clust))
-      estimated_proportions[,clust] = 0.0
+      estimated_proportions$clust <- 0.0
     }
     estimated_proportions <- estimated_proportions[,colnames(actual_proportion)]
   }
