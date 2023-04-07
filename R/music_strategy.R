7e4bdd6ed2ff310600c3b28663f139c6c779eb5e
Jonathan Bard <jbard@buffalo.edu>
2023-04-07 17:11:22 -0400

    updates

--- R/music_strategy.R
+++ R/music_strategy.R
@@ -15,7 +15,7 @@ run_music <- function(ref_object){
   bulk.eset <- music_prep_query(ref_obj = ref_object)
   # Estimate cell type proportions
   print("Running MUSIC ...")
-  estimated.prop = music_prop(bulk.eset = exprs(bulk.eset),
+  estimated.prop = music_prop(bulk.eset = bulk.eset,
                               sc.sce = SC.eset,
                               clusters = 'cellType',
                               samples = 'SubjectName', verbose = T)
