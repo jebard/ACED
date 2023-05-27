e34bd782072b6137c13850c99beea1aeee8d2883
Jonathan Bard <jbard@buffalo.edu>
2023-05-26 23:12:37 -0400

    Update gedit_strategy.R
    updated full path

--- R/gedit_strategy.R
+++ R/gedit_strategy.R
@@ -32,8 +32,8 @@ gedit_prep_query <- function(query_obj){
 
 execute_gedit <- function(res){
   message("Running GEDIT3 using the following settings:")
-  message(paste0(py_config()$python," ",package_info(include_base = F)$library[1],"/GEDIT3.py -mix $PWD/MixtureQuery.csv -ref $PWD/RefObj.",res,".csv -outFile $PWD/GEDIT_Deconv"))
-  system(paste0(py_config()$python," ",package_info(include_base = F)$library[1],"/GEDIT3.py -mix $PWD/MixtureQuery.csv -ref $PWD/RefObj.",res,".csv -outFile $PWD/GEDIT_Deconv"),TRUE)
+  message(paste0(py_config()$python," ",package_info(include_base = F)$library[1],"/ACED/GEDIT3.py -mix $PWD/MixtureQuery.csv -ref $PWD/RefObj.",res,".csv -outFile $PWD/GEDIT_Deconv"))
+  system(paste0(py_config()$python," ",package_info(include_base = F)$library[1],"/ACED/GEDIT3.py -mix $PWD/MixtureQuery.csv -ref $PWD/RefObj.",res,".csv -outFile $PWD/GEDIT_Deconv"),TRUE)
 }
 
 gedit_gather_results <- function(){
