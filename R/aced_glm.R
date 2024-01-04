f85e21aa84e9b740fbb79e9b1b2969390631f837
Jonathan Bard <jbard@buffalo.edu>
2024-01-04 14:57:43 -0500

    added back the lasso loop

--- R/aced_glm.R
+++ R/aced_glm.R
@@ -76,10 +76,14 @@ aced_lasso <- function(ref_obj){
   num_pseudobulk_samples <-ncol(pseudobulk_data)
   num_reference_cell_types <-ncol(reference_data)
 
+  # Initialize a matrix to store proportions
+  proportions <- matrix(0, ncol = num_reference_cell_types, nrow = num_pseudobulk_samples)
+  rownames(proportions) <- colnames(pseudobulk_data)
+  colnames(proportions) <- colnames(reference_data)
   # Loop through each pseudobulk sample
-
+  for (i in 1:num_pseudobulk_samples) {
   # Perform LASSO regression
-  lasso_model <- cv.glmnet(x = reference_data, y = as.vector(pseudobulk_data[,1]), alpha = 1)  # alpha = 1 specifies LASSO regularization
+  lasso_model <- cv.glmnet(x = reference_data, y = as.vector(pseudobulk_data[,i]), alpha = 1)  # alpha = 1 specifies LASSO regularization
 
   # Extract coefficients from the model for the lambda that minimizes cross-validated error
   coefficients <- coef(lasso_model, s = "lambda.min")
@@ -91,16 +95,17 @@ aced_lasso <- function(ref_obj){
   sum_nonzero <- sum(abs(nonzero_coefficients))
 
   # Calculate proportions by normalizing non-zero coefficients
-  proportions <- nonzero_coefficients / sum_nonzero
-
+  sample_proportions <- nonzero_coefficients / sum_nonzero
+  sample_proportions <- as.vector(sample_proportions)
 
 
   #coef_i[coef_i < 0] <- 0 ## if the estimated proportion is less than zero, set to zero.
 
   # Normalize proportions so they sum to 1 for the current sample
   #coef_i <- coef_i / sum(coef_i, na.rm = TRUE)
-
-
+  # Store the proportions in the matrix (column-wise)
+  proportions[i, ] <- sample_proportions
+}
   return(proportions)
 
 }
