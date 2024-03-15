0af9993ae39335306b131aee1d4fee457bea9648
Jonathan Bard <jbard@buffalo.edu>
2024-03-14 22:33:40 -0400

    adding a function to calculate spillover using lasso

--- R/aced_glm.R
+++ R/aced_glm.R
@@ -117,3 +117,59 @@ aced_lasso <- function(ref_obj){
 
 }
 
+aced_lasso_spillover <- function(ref_obj,cluster=cluster){
+  print("Defaulting to ACeD Lasso Deconvolution")
+  # Define the number of pseudobulk samples and reference cell types
+  num_pseudobulk_samples <- length(ref_obj$orig.ident)
+  num_reference_cell_types <- length(ref_obj$seurat_clusters)
+
+  # Simulate pseudobulk single-cell data
+  set.seed(123)  # For reproducibility
+
+  ## Reference Pseudobulk
+  pseudobulk_data <- AverageExpression(ref_obj,assay="RNA",slot = "count",group.by="seurat_clusters")$RNA
+  pseudobulk_data <- pseudobulk_data[,cluster]
+  ## Reference Query by cluster
+  reference_data <- AverageExpression(ref_obj,assays = "RNA",slot = "count",group.by="seurat_clusters")$RNA
+
+  num_pseudobulk_samples <-ncol(pseudobulk_data)
+  num_reference_cell_types <-ncol(reference_data)
+
+  # Initialize a matrix to store proportions
+  proportions <- matrix(0, ncol = num_reference_cell_types, nrow = num_pseudobulk_samples)
+  rownames(proportions) <- colnames(pseudobulk_data)
+  colnames(proportions) <- colnames(reference_data)
+  # Loop through each pseudobulk sample
+  for (i in 1:num_pseudobulk_samples) {
+    # Perform LASSO regression
+    lasso_model <- cv.glmnet(x = reference_data, y = as.vector(pseudobulk_data[,i]), alpha = 1)  # alpha = 1 specifies LASSO regularization
+
+    # Extract coefficients from the model for the lambda that minimizes cross-validated error
+    coefficients <- coef(lasso_model, s = "lambda.min")
+
+    # Extract non-zero coefficients (those selected by LASSO)
+    nonzero_coefficients <- coefficients[-1, , drop = FALSE]
+
+
+    # Set negative coefficients to zero
+    nonzero_coefficients[nonzero_coefficients < 0] <- 0
+
+    # Sum of non-zero coefficients
+    sum_nonzero <- sum(abs(nonzero_coefficients))
+
+    # Calculate proportions by normalizing non-zero coefficients
+    sample_proportions <- nonzero_coefficients / sum_nonzero
+    sample_proportions <- as.vector(sample_proportions)
+
+
+    #coef_i[coef_i < 0] <- 0 ## if the estimated proportion is less than zero, set to zero.
+
+    # Normalize proportions so they sum to 1 for the current sample
+    #coef_i <- coef_i / sum(coef_i, na.rm = TRUE)
+    # Store the proportions in the matrix (column-wise)
+    proportions[i, ] <- sample_proportions
+  }
+  return(proportions)
+
+}
+
